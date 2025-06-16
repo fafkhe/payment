21f1265092970677429cadac08d81c02359e0b8e
fafkhe <115066487+fafkhe@users.noreply.github.com>
2025-06-16 12:18:21 +0330

    feat(payment): add Payment schema to Mongoose module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Payment,PaymentSchema } from './entities/payment.entity';
+
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forFeature([
+      { name: Payment.name, schema: PaymentSchema },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
